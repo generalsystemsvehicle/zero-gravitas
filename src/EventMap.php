b1fcf67a88c368309ca03756f811ed63f8473d5a
Brian Wells <wells@users.noreply.github.com>
2020-04-23 11:10:32 -0500

    Create EventMap.php

---
+++ src/EventMap.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace GeneralSystemsVehicle\Acclaim;
+
+trait EventMap
+{
+    /**
+     * All of the package event / listener mappings.
+     *
+     * @var array
+     */
+    protected $events = [
+        //
+    ];
+}
