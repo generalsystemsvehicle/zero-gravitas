3b6229836b9ea025d59294f674041ac6e212ec15
Brian Wells <wells@users.noreply.github.com>
2020-04-23 11:14:03 -0500

    Create Issuers.php

---
+++ src/Api/Issuers.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace GeneralSystemsVehicle\Acclaim\Api;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\Arr;
+use GeneralSystemsVehicle\Acclaim\Guzzle\Api;
+
+class Issuers extends Api
+{
+    /**
+     * Get the issuers index.
+     * https://www.youracclaim.com/docs/issuer_authorizations#get-issuers
+     *
+     * @param  string $organizationId
+     * @param  array  $payload
+     * @return array|null
+     */
+    public function index($organizationId, $payload = [])
+    {
+        return $this->try(function() use ($organizationId, $payload)
+        {
+            return $this->client->get('v1/organizations/'.$organizationId.'/issuers', [
+                'query' => Arr::get($payload, 'query', []),
+            ]);
+        });
+    }
+
+    /**
+     * Delete / deauthorize an issuer.
+     * https://www.youracclaim.com/docs/issuer_authorizations#deauthorize-an-issuer
+     *
+     * @param  string $organizationId
+     * @param  string $issuerId
+     * @return array|null
+     */
+    public function deauthorize($organizationId, $issuerId)
+    {
+        return $this->delete($organizationId, $issuerId);
+    }
+
+    /**
+     * Delete / deauthorize an issuer.
+     * https://www.youracclaim.com/docs/issuer_authorizations#deauthorize-an-issuer
+     *
+     * @param  string $organizationId
+     * @param  string $issuerId
+     * @return array|null
+     */
+    public function delete($organizationId, $issuerId)
+    {
+        return $this->try(function() use ($organizationId, $issuerId)
+        {
+            return $this->client->delete('v1/organizations/'.$organizationId.'/issuers/'.$issuerId);
+        });
+    }
+}
