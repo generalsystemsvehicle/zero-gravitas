bdebb8893d62fbf6eaeb80f4c462fe3ad97f82a3
Brian Wells <wells@users.noreply.github.com>
2020-04-23 11:13:32 -0500

    Create Grantors.php

---
+++ src/Api/Grantors.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GeneralSystemsVehicle\Acclaim\Api;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\Arr;
+use GeneralSystemsVehicle\Acclaim\Guzzle\Api;
+
+class Grantors extends Api
+{
+    /**
+     * Get the grantors index.
+     * https://www.youracclaim.com/docs/issuer_authorizations#get-grantors
+     *
+     * @param  string $organizationId
+     * @param  array  $payload
+     * @return array|null
+     */
+    public function index($organizationId, $payload = [])
+    {
+        return $this->try(function() use ($organizationId, $payload)
+        {
+            return $this->client->get('v1/organizations/'.$organizationId.'/grantors', [
+                'query' => Arr::get($payload, 'query', []),
+            ]);
+        });
+    }
+}
